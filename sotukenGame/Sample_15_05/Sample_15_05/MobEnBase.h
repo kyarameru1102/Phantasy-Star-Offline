255fe63a363222e7cc7e08573e24dc234254d320
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2020-11-04 10:31:11 +0900

    モブ敵の基底クラスを作成

---
+++ sotukenGame/Sample_15_05/Sample_15_05/MobEnBase.h
@@ -0,0 +1,17 @@
+#pragma once
+/// <summary>
+/// モブ敵の基底クラス
+/// </summary>
+class MobEnBase : public IGameObject
+{
+	/// <summary>
+	/// コンストラクタ。
+	/// </summary>
+	MobEnBase();
+	/// <summary>
+	/// デストラクタ。
+	/// </summary>
+	~MobEnBase();
+
+};
+
