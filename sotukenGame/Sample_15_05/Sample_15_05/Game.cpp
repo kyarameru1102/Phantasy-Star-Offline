d69cca92911f492473a0ab1a77c926a490a667d5
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2020-11-10 16:32:47 +0900

    エラー修正

--- sotukenGame/Sample_15_05/Sample_15_05/Game.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/Game.cpp
@@ -17,10 +17,6 @@ bool Game::Start()
 	//animationClip[0].Load("Assets/animData/unityChan/idle.tka");
 	//animationClip[0].SetLoopFlag(true);
 
-	static AnimationClip gobanimationClip[1];
-	gobanimationClip[0].Load("Assets/animData/goblin/idle01.anim");
-	gobanimationClip[0].SetLoopFlag(true);
-
 	////Unityちゃんのモデルの初期化。
 	//m_ladySkinModelRender = NewGO<SkinModelRender>(0, "UnityChan");
 	//m_ladySkinModelRender->Init("Assets/modelData/unityChan.tkm", animationClip, 1);
@@ -28,9 +24,6 @@ bool Game::Start()
 	//プレイヤー作成。
 	m_player = NewGO<Player>(0, "player");
 	
-	m_goblinSkinModelRender = NewGO<SkinModelRender>(0, "Goblin");
-	m_goblinSkinModelRender->Init("Assets/modelData/enemy/mob/goblin.tkm", gobanimationClip, 1);
-	m_goblinSkinModelRender->PlayAnimation(0, 0.0f);
     return true;
 }
 
