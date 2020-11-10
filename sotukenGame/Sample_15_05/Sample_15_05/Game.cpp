e16d50908101e79cff9bf5d46a537003330d6859
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2020-11-10 12:29:57 +0900

    プレイヤーとアニメーション追加

--- sotukenGame/Sample_15_05/Sample_15_05/Game.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/Game.cpp
@@ -13,15 +13,17 @@ Game::~Game()
 
 bool Game::Start()
 {
-	static AnimationClip animationClip[1];
-	animationClip[0].Load("Assets/animData/unityChan/idle.tka");
-	animationClip[0].SetLoopFlag(true);
+	//static AnimationClip animationClip[1];
+	//animationClip[0].Load("Assets/animData/unityChan/idle.tka");
+	//animationClip[0].SetLoopFlag(true);
 
 
-	//Unityちゃんのモデルの初期化。
-	m_ladySkinModelRender = NewGO<SkinModelRender>(0, "UnityChan");
-	m_ladySkinModelRender->Init("Assets/modelData/unityChan.tkm", animationClip, 1);
-	m_ladySkinModelRender->PlayAnimation(0, 0.0f);
+	////Unityちゃんのモデルの初期化。
+	//m_ladySkinModelRender = NewGO<SkinModelRender>(0, "UnityChan");
+	//m_ladySkinModelRender->Init("Assets/modelData/unityChan.tkm", animationClip, 1);
+	//m_ladySkinModelRender->PlayAnimation(0, 0.0f);
+	//プレイヤー作成。
+	m_player = NewGO<Player>(0, "player");
 	
     return true;
 }
