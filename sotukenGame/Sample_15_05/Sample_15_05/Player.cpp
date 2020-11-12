21e61bc6a2055caf16c2f5b69fc6c2571a086a2d
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2020-11-12 13:08:31 +0900

    プレイヤーをコントローラー基準で操作するようにした プレイヤーをターゲットにカメラを動かすようにした カメラの上下の移動に制限をつけた

--- sotukenGame/Sample_15_05/Sample_15_05/Player.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/Player.cpp
@@ -3,18 +3,6 @@
 
 Player::Player()
 {
-	////アニメーションをロード。
-	//animClip[enStay01].Load("Assets/animData/player/blad/stay_01.tka");
-	//animClip[enStay02].Load("Assets/animData/player/sword/stay_02.tka");
-	//animClip[enStay01].SetLoopFlag(true);
-	//animClip[enStay02].SetLoopFlag(true);
-	////モデルの初期化。
-	//m_playerSkinModel = NewGO<SkinModelRender>(0);
-	//m_playerSkinModel->Init("Assets/modelData/player/player.tkm", animClip, enAnimationClipNum);
-	//Quaternion qRot;
-	//qRot.SetRotationDegX(0.0f);
-	//m_playerSkinModel->SetRotation(qRot);
-	//m_charaCon.Init(50.0f, 100.0f, m_position);
 }
 
 Player::~Player()
@@ -39,22 +27,21 @@ bool Player::Start()
 }
 void Player::Update()
 {
-	m_moveSpeed = Vector3::Zero;
-	if (GetAsyncKeyState(VK_UP)) {
-		m_moveSpeed.z = 5.0f;
-	}
-	if (GetAsyncKeyState(VK_DOWN)) {
-		m_moveSpeed.z = -5.0f;
-	}
-	if (GetAsyncKeyState(VK_RIGHT)) {
-		m_moveSpeed.x = 5.0f;
-	}
-	if (GetAsyncKeyState(VK_LEFT)) {
-		m_moveSpeed.x = -5.0f;
-	}
-
+	//プレイヤーのムーブスピードを更新。
+	m_moveSpeed.x = g_pad[0]->GetLStickXF() * -5.0f;
+	m_moveSpeed.z = g_pad[0]->GetLStickYF() * -5.0f;
+	//座標を設定。
 	m_position = m_charaCon.Execute(1.0f, m_moveSpeed);
 	m_playerSkinModel->SetPosition(m_position);
+
+	if (
+		fabsf(m_moveSpeed.z) > 0.0f &&
+		fabsf(m_moveSpeed.x) > 0.0f) {
+		float rot = atan2f(m_moveSpeed.x, m_moveSpeed.z);
+		m_rotation.SetRotation(Vector3::AxisY, rot);
+	}
+	m_playerSkinModel->SetRotation(m_rotation);
+
 
 	if (GetAsyncKeyState('B')) {
 		//ブレードモード。
