0efac356e1c581b2cbcc67c2f732341a167b2ffd
kbc18b19 <kbc18b19@stu.kawahara.ac.jp>
2020-11-13 11:42:21 +0900

    背景仮地面設置

--- sotukenGame/Sample_15_05/Sample_15_05/BackGround.h
+++ sotukenGame/Sample_15_05/Sample_15_05/BackGround.h
@@ -13,11 +13,11 @@ public:
 	void Update()override;
 private:
 	SkinModelRender* m_bgModel = nullptr;
-	//BoxCollider m_box;
+	BoxCollider m_box;
 	MeshCollider m_mesh;
 	RigidBody m_rigidBody;
-	Vector3 m_position = Vector3::Zero;
+	Vector3 m_position = Vector3(0.0f,-50.0f,0.0f);
 	Quaternion m_rotation = Quaternion::Identity;
-	Vector3 m_scale = Vector3::One;
+	Vector3 m_scale = Vector3(1000.0f,100.0f,1000.0f);
 };
 
