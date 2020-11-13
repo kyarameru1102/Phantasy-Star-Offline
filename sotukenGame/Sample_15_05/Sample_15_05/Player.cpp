ed85801ec7a92d8a4b0d96ebd1688a4c63282154
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2020-11-13 11:24:20 +0900

    アニメーション調整中

--- sotukenGame/Sample_15_05/Sample_15_05/Player.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/Player.cpp
@@ -16,10 +16,15 @@ bool Player::Start()
 	animClip[enStay02].Load("Assets/animData/player/sword/stay_02.tka");
 	animClip[enChange01].Load("Assets/animData/player/blad/change_01.tka");
 	animClip[enChange02].Load("Assets/animData/player/sword/change_02.tka");
+	animClip[attackAndChang01].Load("Assets/animData/player/blad/attackAndChange_01.tka");
+	animClip[attackAndChang02].Load("Assets/animData/player/sword/attackAndChange_02.tka");
 	animClip[enStay01].SetLoopFlag(true);
 	animClip[enStay02].SetLoopFlag(true);
 	animClip[enChange01].SetLoopFlag(true);
 	animClip[enChange02].SetLoopFlag(true);
+	animClip[attackAndChang01].SetLoopFlag(true);
+	animClip[attackAndChang02].SetLoopFlag(true);
+
 
 	//モデルの初期化。
 	m_playerSkinModel = NewGO<SkinModelRender>(0);
@@ -58,10 +63,15 @@ void Player::Update()
 	m_playerSkinModel->SetRotation(m_rotation);
 
 
-	if (g_pad[0]->IsPress(enButtonX)) {
+	if (g_pad[0]->IsTrigger(enButtonX)) {
 		//武器変更のフラグを立てる。
 		m_changeAnimFlag = true;
+		//m_attackAndChangState = enChange01;
 	}
+	/*else if (g_pad[0]->IsPress(enButtonY)) {
+		m_changeAnimFlag = true;
+		m_attackAndChangState = attackAndChang01;
+	}*/
 	if (m_changeAnimFlag != false) {
 		//武器の状態によって武器変更のアニメーションを決める。
 		if (m_weaponState == enBladState) {
