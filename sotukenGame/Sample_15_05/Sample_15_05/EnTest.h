c8d0ba9dd7f75fb2b740c8dd1c65917550ff9496
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2020-11-25 11:39:33 +0900

    EnArmyGeneratorクラスを作成

--- sotukenGame/Sample_15_05/Sample_15_05/EnTest.h
+++ sotukenGame/Sample_15_05/Sample_15_05/EnTest.h
@@ -14,8 +14,16 @@ private:
 	Quaternion m_rotation = Quaternion::Identity;
 	enum EnAnimationClip {
 		enIdle,
+		enWalk,
+		enRun,
+		enScream,
+		enAttack,
+		enHornattack,
+		enGethit,
+		enDie,
 		enAnimationClip_num
 	};
+	int changeTimer = 0;
 	int m_animState = enIdle; //アニメーションの状態。
 	AnimationClip m_animationClip[enAnimationClip_num];
 };
