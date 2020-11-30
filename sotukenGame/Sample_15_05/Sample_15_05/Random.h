9a5401b6698245d1c8c1d164a2c987c01c83ce84
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2020-11-30 11:36:06 +0900

    メンバ変数を追加

--- sotukenGame/Sample_15_05/Sample_15_05/Random.h
+++ sotukenGame/Sample_15_05/Sample_15_05/Random.h
@@ -1,7 +1,25 @@
 #pragma once
 class Random
 {
+	/// <summary>
+	/// コンストラクタ。
+	/// </summary>
 	Random();
+	/// <summary>
+	/// デストラクタ。
+	/// </summary>
 	~Random();
+	/*!
+	 * @brief	乱数を初期化。
+	 *@param[in]	seed		ランダムシード。
+	 */
+
+private:
+	static const unsigned long N = 624;
+	static const unsigned long M = 397;
+	static const unsigned long MATRIX_A = 0x9908b0dfUL;   /* constant vector a */
+	static const unsigned long UPPER_MASK = 0x80000000UL; /* most significant w-r bits */
+	static const unsigned long LOWER_MASK = 0x7fffffffUL; /* least significant r bits */
+
 };
 
