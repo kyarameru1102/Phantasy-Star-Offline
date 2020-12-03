10941922667085b046f4397e34fa17baa947b830
iwamoto takuya <kbc18b04@stu.kawahara.ac.jp>
2020-12-03 13:35:25 +0900

    とりあえずエネミーをだした

--- sotukenGame/Sample_15_05/Sample_15_05/Game.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/Game.cpp
@@ -24,6 +24,7 @@ bool Game::Start()
 	m_gameCam = NewGO<GameCamera>(0, "gameCamera");
 	m_bg = NewGO<BackGround>(0, "backGround");
 	m_playerStatusUI = NewGO<PlayerStatusUI>(0);
+	m_drBoar = NewGO<DrBoar>(0, "drBoar");
     return true;
 }
 
