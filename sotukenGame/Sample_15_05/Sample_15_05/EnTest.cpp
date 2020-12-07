276fc8b5075f7372d1a3875c3177bcd000491f83
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2020-12-07 14:00:37 +0900

    コライダーのサイズを変更

--- sotukenGame/Sample_15_05/Sample_15_05/EnTest.cpp
+++ sotukenGame/Sample_15_05/Sample_15_05/EnTest.cpp
@@ -39,7 +39,7 @@ bool EnTest::Start()
 	m_position = { 300.0f, 0.0f, 100.0f };
 	m_rotation.SetRotationDegY(90.0f);
 	//ƒLƒƒƒ‰ƒRƒ“‰Šú‰»B
-	m_charaCon.Init(200.0f, 150.0f, m_position);
+	m_charaCon.Init(145.0f, 200.0f, m_position);
 	return true;
 }
 void EnTest::Move()
