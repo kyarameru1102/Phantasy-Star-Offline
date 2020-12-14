f6bf5e1f3aaca389aa911679156104ceba256533
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2020-12-14 10:07:43 +0900

    キャラコン取得の関数を変更

---
+++ sotukenGame/Sample_15_05/Sample_15_05/EnBase.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "EnBase.h"
+
+EnBase::EnBase()
+{
+}
+
+EnBase::~EnBase()
+{
+}
+
---
+++ sotukenGame/Sample_15_05/Sample_15_05/EnBase.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Physics/Character/CharacterController.h"
+/// <summary>
+/// 敵基底クラス
+/// </summary>
+class Player;
+class EnBase : public IGameObject
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	EnBase();
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~EnBase();
+
+	
+};
+
